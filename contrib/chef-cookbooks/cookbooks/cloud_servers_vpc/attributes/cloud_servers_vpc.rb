a39a8b9d1454e50fee13dad91c8fc7b6f8909969
Dan Prince <dan.prince@rackspace.com>
2010-11-18 16:01:56 -0500

    Updated Chef cookbook for Git Hub deployment.

--- contrib/chef-cookbooks/cookbooks/cloud_servers_vpc/attributes/cloud_servers_vpc.rb
+++ contrib/chef-cookbooks/cookbooks/cloud_servers_vpc/attributes/cloud_servers_vpc.rb
@@ -1,4 +1,4 @@
-default[:cloud_servers_vpc][:version]=nil #optional
+default[:cloud_servers_vpc][:version]="master"
 
 default[:cloud_servers_vpc][:rackspace_cloud_username]="test"
 default[:cloud_servers_vpc][:rackspace_cloud_api_key]="test"
@@ -6,3 +6,5 @@ default[:cloud_servers_vpc][:rackspace_cloud_api_key]="test"
 default[:cloud_servers_vpc][:rackspace_cloud_server_name_prefix]="" #optional
 
 set[:cloud_servers_vpc][:home]="/opt/cloud-servers-vpc"
+
+set[:cloud_servers_vpc][:delayed_job_workers]="8"
--- contrib/chef-cookbooks/cookbooks/cloud_servers_vpc/recipes/default.rb
+++ contrib/chef-cookbooks/cookbooks/cloud_servers_vpc/recipes/default.rb
@@ -5,18 +5,13 @@
 # Copyright 2010, Rackspace
 #
 
-package "rails-stack" do
-  action :install
-  version "2.3.8"
-end
+include_recipe "ruby_enterprise"
+include_recipe "apache2"
 
-package "cloud-servers-vpc" do
-  action :install
-  version node[:cloud_servers_vpc][:version] if node[:cloud_servers_vpc][:version]
-end
-
-package "mysql-server" do
-  action :install
+["mysql-server","mysql-devel"].each do |pkg|
+  package pkg do
+    action :install
+  end
 end
 
 service "mysqld" do
@@ -24,9 +19,50 @@ service "mysqld" do
   action [ :enable, :start ]
 end
 
-service "httpd" do
-  supports :status => true, :restart => true, :reload => true
-  action [ :enable, :start ]
+[
+  ['rake','0.8.7'],
+  ['rails','2.3.8'],
+  ['mysql','2.8.1'],
+  ['json','1.4.3'],
+  ['daemons','1.1.0']
+].each do |gem_version|
+
+  ree_gem gem_version[0] do
+    version gem_version[1]
+  end
+
+end
+
+user "cloud_servers_vpc" do
+  comment "Cloud Servers VPC"
+  system true
+  shell "/bin/false"
+end
+
+version_check_file="#{node[:cloud_servers_vpc][:home]}/version_#{node[:cloud_servers_vpc][:version]}"
+bash "download_and_extract" do
+  action :run
+  cwd "/tmp"
+  code <<-EOH
+    mkdir -p #{node[:cloud_servers_vpc][:home]}
+    cd #{node[:cloud_servers_vpc][:home]}
+    wget --no-check-certificate https://github.com/rackspace/cloud_servers_vpc/tarball/#{node[:cloud_servers_vpc][:version]} -O - | tar -xz
+    mv rackspace-*/* .
+    rm -Rf rackspace*
+    cd /opt && chown cloud_servers_vpc:cloud_servers_vpc -R cloud-servers-vpc
+    touch #{version_check_file}
+  EOH
+  not_if do File.exists?(version_check_file) end
+end
+
+["/opt/cloud-servers-vpc/tmp/pids", "/opt/cloud-servers-vpc/tmp/ssh_keys"].each do |tmp_dir|
+  directory tmp_dir do
+    owner "cloud_servers_vpc"
+    group "cloud_servers_vpc"
+    mode "0755"
+    action :create
+    recursive true
+  end
 end
 
 bash "create_update_db" do
@@ -35,15 +71,33 @@ bash "create_update_db" do
   user "root"
   code <<-EOH
     cd #{node[:cloud_servers_vpc][:home]}
-    RAILS_ENV=production rails-stack-ruby /usr/lib/rails-stack-gems/bin/rake db:create
-    RAILS_ENV=production rails-stack-ruby /usr/lib/rails-stack-gems/bin/rake db:migrate
+    RAILS_ENV=production /opt/ruby-enterprise/bin/ruby /opt/ruby-enterprise/bin/rake db:create
+    RAILS_ENV=production /opt/ruby-enterprise/bin/ruby /opt/ruby-enterprise/bin/rake db:migrate
   EOH
-  subscribes :run, resources(:package => "cloud-servers-vpc")
+  subscribes :run, resources("bash[download_and_extract]")
+end
+
+cookbook_file "/etc/pki/tls/private/ca.key" do
+  source "ca.key"
+  mode 0600
+end
+
+cookbook_file "/etc/pki/tls/certs/ca.crt" do
+  source "ca.crt"
+  mode 0600
+end
+
+package "monit"
+
+template "/etc/monit.d/cloud_servers_vpc.monit" do
+  mode "0644"
+  source "cloud_servers_vpc.monit.erb"
 end
 
 service "monit" do
   supports :status => true, :restart => true, :reload => true
   action [ :enable, :start ]
+  subscribes :restart, resources("template[/etc/monit.d/cloud_servers_vpc.monit]")
 end
 
 bash "stop_delayed_job" do
@@ -51,10 +105,10 @@ bash "stop_delayed_job" do
   cwd "/tmp"
   user "root"
   code <<-EOH
-	/usr/bin/cloud-servers-vpc-stop-dj || /bin/true
+      /usr/bin/cloud-servers-vpc-stop-dj || /bin/true
   EOH
-  notifies :restart, resources("service[monit]", "service[httpd]")
-  subscribes :run, resources(:package => "cloud-servers-vpc")
+  notifies :restart, resources("service[monit]", "service[apache2]")
+  subscribes :run, resources("bash[download_and_extract]")
 end
 
 template "#{node[:cloud_servers_vpc][:home]}/config/environments/production.rb" do
@@ -63,4 +117,22 @@ template "#{node[:cloud_servers_vpc][:home]}/config/environments/production.rb" 
   mode "0644"
   source "production.rb.erb"
   notifies :run, resources(:bash => "stop_delayed_job")
+end
+
+web_app "cloud_servers_vpc" do
+  docroot "/opt/cloud-servers-vpc/public"
+  server_name "vpc.#{node[:domain]}"
+  server_aliases [ "vpc", node[:hostname] ]
+  rails_env "production"
+end
+
+execute "touch_cron_d" do
+  command "touch /etc/cron.d"
+  action :nothing
+end
+
+template "/etc/cron.d/cloud_servers_vpc" do
+  mode "0644"
+  source "cloud_servers_vpc.cron.erb"
+  notifies :run, resources("execute[touch_cron_d]")
 end
