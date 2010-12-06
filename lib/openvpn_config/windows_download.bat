3dd0851be0b2c3c13f913d94b989a956dd739f28
Dan Prince <dan.prince@rackspace.com>
2010-12-06 10:06:02 -0500

    Remove Wscript echo. Set HTTP timeout.

--- lib/openvpn_config/windows_download.bat
+++ lib/openvpn_config/windows_download.bat
@@ -3,14 +3,14 @@ cd c:\
 ECHO off
 
 ECHO Function download^(Url, File^) > script.vbs
-ECHO WScript.Echo "Downloading " ^& Url >> script.vbs
-ECHO Set Http = CreateObject^("WinHttp.WinHttpRequest.5.1"^) >> script.vbs
-ECHO Http.Open "GET", Url, False >> script.vbs
-ECHO Http.Send >> script.vbs
+ECHO Set WinHttpReq = CreateObject^("WinHttp.WinHttpRequest.5.1"^) >> script.vbs
+ECHO WinHttpReq.SetTimeouts 30000, 30000, 70000, 70000 >> script.vbs
+ECHO WinHttpReq.Open "GET", Url, False >> script.vbs
+ECHO WinHttpReq.Send >> script.vbs
 ECHO Set BinaryStream = CreateObject^("ADODB.Stream"^) >> script.vbs
 ECHO BinaryStream.Type = 1 >> script.vbs
 ECHO BinaryStream.Open >> script.vbs
-ECHO BinaryStream.Write Http.ResponseBody >> script.vbs
+ECHO BinaryStream.Write WinHttpReq.ResponseBody >> script.vbs
 ECHO BinaryStream.SaveToFile File, 2 >> script.vbs
 ECHO BinaryStream.Close >> script.vbs
 ECHO END Function >> script.vbs
@@ -19,4 +19,4 @@ ECHO download "http://c2865862.cdn.cloudfiles.rackspacecloud.com/openvpn-2.1.3-i
 
 ECHO download "http://c2865862.cdn.cloudfiles.rackspacecloud.com/AutoIt3_x64.exe", "AutoIt3.exe" >> script.vbs
 
-cscript //nologo c:/script.vbs
+cscript c:\script.vbs
