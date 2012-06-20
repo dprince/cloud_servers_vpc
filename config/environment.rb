54f415d3c916098e21177cdedce21ba57c082916
Dan Prince <dprince@redhat.com>
2012-06-20 15:29:02 -0400

    Updating version information for tag: 3.0.1

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 CloudServersVpc::Application.initialize!
 
-CLOUD_SERVERS_VPC_VERSION="0.0-master"
+CLOUD_SERVERS_VPC_VERSION="3.0.1"
 
 # load in the jobs classes
 Dir[File.join(File.dirname(__FILE__), '..', 'app', 'models', 'jobs', '*')].each do |job|
